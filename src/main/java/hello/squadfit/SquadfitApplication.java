1ff9be9ce4467b83b6e62368cdfb434d05509e86
jaewoo <202017499@jbnu.ac.kr>
2025-07-24 04:22:01 +0900

    init

---
+++ src/main/java/hello/squadfit/SquadfitApplication.java
@@ -0,0 +1,13 @@
+package hello.squadfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquadfitApplication.class, args);
+    }
+
+}
