62e5540c20623ee408cedf570d7aa82a4932786f
jaewoo-rain <202017499@jbnu.ac.kr>
2025-08-31 06:49:45 +0900

    ss

---
+++ src/main/java/hello/squadfit/api/report/controller/ReportController.java
@@ -0,0 +1,18 @@
+package hello.squadfit.api.report.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/report")
+public class ReportController {
+
+    // 레포트 신청하기
+
+    // 레포트 만들기
+
+
+
+}
